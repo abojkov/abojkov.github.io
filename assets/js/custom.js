195e72acf13399ef3db8bfa3fce298d8f2bee7e2
abojkov <ace.bojkov11@gmail.com>
2023-01-13 18:33:43 +0100

    Project migration update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,4 +32,48 @@
     $('.navbar-toggler').click();
   })
 
+  // Initiate venobox (lightbox feature used in portofilo)
+  new VenoBox({
+    'share': false,
+    'height': '100%'
+  });
+
+  // Portfolio details carousel
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Include html functionality from W3Schools
+  function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain attribute:*/
+      file = elmnt.getAttribute("data-htmlinclude");
+      if (file) {
+        /*make an HTTP request using the attribute value as the file name:*/
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState === 4) {
+            if (this.status === 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status === 404) {elmnt.innerHTML = "Page not found.";}
+            /*remove the attribute, and call this function once more:*/
+            elmnt.removeAttribute("data-htmlinclude");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /*exit the function:*/
+        return;
+      }
+    }
+  };
+  includeHTML()
+
 })(window.jQuery);
